29b57427084c92dabf24ee65fa3dffaf87349d5e
kh4ydarov <kh4ydarovs@yandex.ru>
2024-05-26 21:38:59 +0500

    hw

--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -15,15 +15,15 @@ def test_search_samarkand():
         results.first.should(have.text('Samarkand'))
 
 
-def test_search_tashkent():
+def test_search_uzbekistan():
 
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Tashkent')
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Uzbekistan')
 
     with step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
-        results.first.should(have.text('Tashkent'))
+        results.first.should(have.text('Uzbekistan'))
         results.first.click()
 
