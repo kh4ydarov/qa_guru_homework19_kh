eced880c9f520a965cd2cf7c1a1a63bf3861ec30
kh4ydarov <kh4ydarovs@yandex.ru>
2024-05-26 21:52:18 +0500

    hw

--- tests/android_app/test_wikipedia.py
+++ tests/android_app/test_wikipedia.py
@@ -19,7 +19,7 @@ def test_search_samsung():
 
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('samsung')
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Samsung')
 
     with step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
